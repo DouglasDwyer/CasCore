cabb31d9e6d007e5672857f7a2e358f1c7a075a8
Douglas Dwyer <dougmdwyer@hotmail.com>
2024-10-20 11:02:43 -0400

    Begin work on support for System.Linq.Expressions

---
+++ CasCore/GuardExpressionVisitor.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+using System.Security;
+
+namespace DouglasDwyer.CasCore;
+
+internal class GuardExpressionVisitor : ExpressionVisitor
+{
+    private readonly Assembly _assembly;
+
+    protected override Expression VisitBinary(BinaryExpression node)
+    {
+        if (node.Method is not null)
+        {
+            return Expression.MakeBinary(
+                node.NodeType,
+                Visit(node.Left),
+                Visit(node.Right),
+                node.IsLiftedToNull,
+                GuardMethodInfo.Create(_assembly, node.Method),
+                (LambdaExpression?)Visit(node.Conversion)
+            );
+        }
+        else
+        {
+            return base.VisitBinary(node);
+        }
+    }
+
+    protected override Expression VisitDynamic(DynamicExpression node)
+    {
+        throw new SecurityException("Compiling System.Linq.Expressions.DynamicExpression is not supported in CAS contexts.");
+    }
+
+    protected override ElementInit VisitElementInit(ElementInit node)
+    {
+        return base.VisitElementInit(node);
+    }
+
+    protected override Expression VisitIndex(IndexExpression node)
+    {
+        return base.VisitIndex(node);
+    }
+
+    protected override Expression VisitMember(MemberExpression node)
+    {
+        return base.VisitMember(node);
+    }
+
+    protected override MemberAssignment VisitMemberAssignment(MemberAssignment node)
+    {
+        return base.VisitMemberAssignment(node);
+    }
+
+    protected override MemberBinding VisitMemberBinding(MemberBinding node)
+    {
+        return base.VisitMemberBinding(node);
+    }
+
+    protected override MemberListBinding VisitMemberListBinding(MemberListBinding node)
+    {
+        return base.VisitMemberListBinding(node);
+    }
+
+    protected override MemberMemberBinding VisitMemberMemberBinding(MemberMemberBinding node)
+    {
+        return base.VisitMemberMemberBinding(node);
+    }
+
+    protected override Expression VisitMethodCall(MethodCallExpression node)
+    {
+        return base.VisitMethodCall(node);
+    }
+
+    protected override Expression VisitNew(NewExpression node)
+    {
+        return base.VisitNew(node);
+    }
+
+    protected override Expression VisitSwitch(SwitchExpression node)
+    {
+        return base.VisitSwitch(node);
+    }
+
+    protected override Expression VisitUnary(UnaryExpression node)
+    {
+        return base.VisitUnary(node);
+    }
+}
---
+++ CasCore/GuardMethodInfo.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace DouglasDwyer.CasCore;
+
+internal class GuardMethodInfo : MethodInfo
+{
+    private readonly Assembly _assembly;
+    private readonly MethodInfo _inner;
+
+    public override ICustomAttributeProvider ReturnTypeCustomAttributes => _inner.ReturnTypeCustomAttributes;
+
+    public override MethodAttributes Attributes => _inner.Attributes;
+
+    public override RuntimeMethodHandle MethodHandle => throw new NotSupportedException();
+
+    public override Type? DeclaringType => _inner.DeclaringType;
+
+    public override string Name => _inner.Name;
+
+    public override Type? ReflectedType => _inner.ReflectedType;
+
+    private GuardMethodInfo(Assembly assembly, MethodInfo inner)
+    {
+        _assembly = assembly;
+        _inner = inner;
+    }
+
+    public static MethodInfo Create(Assembly assembly, MethodInfo inner)
+    {
+        if (CasAssemblyLoader.CanCallAlways(assembly, inner))
+        {
+            return inner;
+        }
+        else
+        {
+            return new GuardMethodInfo(assembly, inner);
+        }
+    }
+
+    public override MethodInfo GetBaseDefinition()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override object[] GetCustomAttributes(bool inherit)
+    {
+        return _inner.GetCustomAttributes(inherit);
+    }
+
+    public override object[] GetCustomAttributes(Type attributeType, bool inherit)
+    {
+        return _inner.GetCustomAttributes(attributeType, inherit);
+    }
+
+    public override MethodImplAttributes GetMethodImplementationFlags()
+    {
+        return _inner.GetMethodImplementationFlags();
+    }
+
+    public override ParameterInfo[] GetParameters()
+    {
+        return _inner.GetParameters();
+    }
+
+    public override object? Invoke(object? obj, BindingFlags invokeAttr, Binder? binder, object?[]? parameters, CultureInfo? culture)
+    {
+        CasAssemblyLoader.AssertCanCall(_assembly, obj, _inner);
+        return _inner.Invoke(obj, invokeAttr, binder, parameters, culture);
+    }
+
+    public override bool IsDefined(Type attributeType, bool inherit)
+    {
+        return _inner.IsDefined(attributeType, inherit);
+    }
+}
