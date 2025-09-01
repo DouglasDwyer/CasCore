cfb5eb65face188bb34d9ef64d533f95ea6d1492
Douglas M. Dwyer <dougmdwyer@hotmail.com>
2025-09-01 09:02:40 -0400

    Add unit test for UnsafeAccessor

---
+++ CasCore.Tests/TestUnsafeAccessor.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security;
+
+namespace DouglasDwyer.CasCore.Tests;
+
+public static class TestUnsafeAccessors
+{
+    [TestException(typeof(InvalidProgramException))]
+    public static void TestNoUnsafeAccessors()
+    {
+        var foo = new Foo();
+
+        string hidden = GetHiddenField(foo);
+        
+        if (hidden == "foo")
+        {
+            throw new Exception("Test failed");
+        }
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_hiddenField")]
+    private static extern string GetHiddenField(Foo person);
+
+    private class Foo
+    {
+        public string HiddenValue => _hiddenField;
+
+        private string _hiddenField = "foo";
+    }
+}
